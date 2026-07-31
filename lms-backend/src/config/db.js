d75fb4e6659603918351bfdb46d96ad9cdc71602
RunalyiFSD <salunkherunalyi@gmail.com>
2026-07-31 17:01:30 +0530

    Fix issuses in db.js and package.jason

--- lms-backend/src/config/db.js
+++ lms-backend/src/config/db.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
+const dns = require('dns');
 const seedMocks = require('../utils/mockSeeder');
 const logger = require('../utils/logger');
+
+// Use Google DNS to resolve MongoDB Atlas SRV records
+// (fixes issues with routers that don't support SRV lookups)
+dns.setServers(['8.8.8.8', '8.8.4.4']);
 
 const connectDB = async () => {
   try {
